f1bdd15c0d478838a3aaa5677f5e253847f979a2
ColeBo <ColeBo@COLEBO-PC.Magenic.net>
2017-03-09 16:09:33 -0600

    Initial Commit

---
+++ jmaqs-baseTest/src/main/java/com/magenic/jmaqs/baseTest/TestObject.java
@@ -0,0 +1,66 @@
+package com.magenic.jmaqs.baseTest;
+
+import magenic.maqs.utilities.Logging.Logger;
+
+/**
+ * The TestObject class
+ * 
+ * @author ColeBo
+ */
+public class TestObject
+{
+	/**
+	 * The Logger object
+	 */
+	protected Logger logger;
+	
+	/**
+	 * The Fully Qualified Test Name
+	 */
+	protected String testName;
+
+	/**
+	 * Initializes a new instance of the TestObject class
+	 * 
+	 * @param fullyQualifiedTestName
+	 * 			  The Fully Qualified Test Name
+	 * @param logger
+	 * 			  The Logger object
+	 */
+	public TestObject(String fullyQualifiedTestName, Logger logger)
+	{
+		this.testName = fullyQualifiedTestName;
+		this.logger = logger;
+	}
+	
+	
+	/**
+	 * Get the Fully Qualified Test Name
+	 * @return
+	 */
+	public String getFullyQualifiedTestName()
+	{
+		return testName;
+	}
+
+	/**
+	 * Get the Logger
+	 * 
+	 * @return A Logger object
+	 */
+	public Logger getLogger()
+	{
+		return this.logger;
+	}
+
+	/**
+	 * Set the Logger for the TestObject
+	 * 
+	 * @param log
+	 *            The Logger object
+	 */
+	public void setLogger(Logger log)
+	{
+		this.logger = log;
+	}
+}
---
+++ jmaqs-baseTest/src/test/java/com/magenic/jmaqs/baseTest/unitTests/BaseGenericTestUnitTest.java
@@ -0,0 +1,62 @@
+package com.magenic.jmaqs.baseTest.unitTests;
+
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.Test;
+
+import com.magenic.jmaqs.baseTest.BaseGenericTest;
+
+import magenic.maqs.utilities.Logging.FileLogger;
+
+/**
+ * Unit test class for BaseGenericTest class
+ * 
+ * @author ColeBo
+ */
+@Test
+public class BaseGenericTestUnitTest extends BaseGenericTest
+{
+	/**
+	 * Verify fully qualified test name
+	 */
+	@Test
+	public void fullyQualifiedTestNameTest()
+	{
+		String testName = this.getFullyQualifiedTestClassName();
+		
+		Assert.assertEquals(testName, "magenic.maqs.utilities.unitTests.BaseGenericTestUnitTest.fullyQualifiedTestNameTest");
+	}
+	
+	/**
+	 * Validate setting a new logger
+	 */
+	@Test
+	public void FileLoggerTest()
+	{
+		this.setLogger(new FileLogger());
+		
+		if (!(this.getLogger() instanceof FileLogger))
+		{
+			Assert.fail("FileLogger was not set.");
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see magenic.maqs.utilities.BaseTest.BaseGenericTest#postSetupLogging()
+	 */
+	@Override
+	protected void postSetupLogging()
+	{
+	
+	}
+
+	/* (non-Javadoc)
+	 * @see magenic.maqs.utilities.BaseTest.BaseGenericTest#beforeLoggingTeardown(org.testng.ITestResult)
+	 */
+	@Override
+	protected void beforeLoggingTeardown(ITestResult resultType)
+	{
+		
+	}
+
+}
