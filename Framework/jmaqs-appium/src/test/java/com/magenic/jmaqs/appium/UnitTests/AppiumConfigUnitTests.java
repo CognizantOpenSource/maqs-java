c4e16781b56d57558e3c671d24e15d6841a7b6e2
Jason Edstrom <JasonE@magenic.com>
2017-05-30 03:16:15 -0500

    Package renaming pt. 2

---
+++ Framework/jmaqs-appium/src/test/java/com/magenic/jmaqs/appium/UnitTests/AppiumConfigUnitTests.java
@@ -0,0 +1,67 @@
+package com.magenic.jmaqs.appium.UnitTests;
+
+
+import com.magenic.jmaqs.appium.baseAppiumTest.AppiumConfig;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class AppiumConfigUnitTests {
+//    @Test
+//    public void testGetMobileDeviceOS() throws Exception {
+//        String mobileDeviceOS = AppiumConfig.getMobileDeviceOs();
+//        Assert.assertTrue(mobileDeviceOS.equalsIgnoreCase("Android"));
+//
+//    }
+
+    @Test
+    public void testGetMobileDeviceUDID() throws Exception {
+        String mobileDeviceUDID = AppiumConfig.getMobileDeviceUdid();
+        Assert.assertTrue(mobileDeviceUDID.equalsIgnoreCase("1234567890ACDEF1234687890ABCDEF"));
+    }
+
+    @Test
+    public void testGetBundleID() throws Exception {
+        String bundleID = AppiumConfig.getBundleId();
+        Assert.assertTrue(bundleID.equalsIgnoreCase("com.magenic.maqs.appium.tester"));
+    }
+
+    @Test
+    public void testGetOSVersion() throws Exception {
+        String osVersion = AppiumConfig.getOsVersion();
+        Assert.assertTrue(osVersion.equalsIgnoreCase("6.0"));
+    }
+
+    @Test
+    public void testGetDeviceName() throws Exception {
+        String deviceName = AppiumConfig.getDeviceName();
+        Assert.assertTrue(deviceName.equalsIgnoreCase("Samsung Galaxy S6 Device"));
+    }
+
+    /*@Test
+    public void testMobileDevice() throws Exception {
+        AppiumDriver driver = AppiumConfig.mobileDevice();
+        Assert.assertNotNull(driver);
+        driver.quit();
+    }*/
+
+//    @Test
+//    public void testIsUsingMobileBrowser() throws Exception {
+//        boolean mobileBrowser = AppiumConfig.isUsingMobileBrowser();
+//        Assert.assertFalse(mobileBrowser);
+//    }
+
+    @Test
+    public void testGetMobileHubUrlString() throws Exception {
+        String mobileHubUrl = AppiumConfig.getMobileHubUrlString();
+        Assert.assertTrue(mobileHubUrl.equalsIgnoreCase("http://127.0.0.1:4723/wd/hub"));
+    }
+
+
+    @Test
+    public void testSetTimeouts() throws Exception {
+
+    }
+
+
+}
