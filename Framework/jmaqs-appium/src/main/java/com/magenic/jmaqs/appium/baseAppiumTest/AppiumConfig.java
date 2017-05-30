886f6b1a338cc1ae05f6c2daf7e5ba32a0a2969b
Jason Edstrom <JasonE@magenic.com>
2017-05-30 01:18:20 -0500

    Adding files back under correct package structure

---
+++ Framework/jmaqs-appium/src/main/java/com/magenic/jmaqs/appium/baseAppiumTest/AppiumConfig.java
@@ -0,0 +1,219 @@
+/* 
+ * Copyright 2017 (C) Magenic, All rights Reserved
+ */
+
+package com.magenic.maqs.appium.baseAppiumTest;
+
+import com.magenic.jmaqs.utilities.helper.Config;
+import com.magenic.jmaqs.utilities.helper.StringProcessor;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * Class AppiumConfig ...
+ *
+ * @author jasonedstrom Created on 5/30/17
+ */
+public class AppiumConfig {
+
+  /**
+   * Gets the mobile device OS.
+   *
+   * @return the mobile device OS
+   */
+  public static String getMobileDeviceOs() {
+    return Config.getValue("MobileOSType", "Android");
+  }
+
+  /**
+   * Gets the mobile device UDID.
+   *
+   * @return the mobile device UDID
+   */
+  public static String getMobileDeviceUdid() {
+    return Config.getValue("DeviceUDID");
+  }
+
+  /**
+   * Gets the bundle ID.
+   *
+   * @return the bundle ID
+   */
+  public static String getBundleId() {
+    return Config.getValue("BundleID");
+  }
+
+  /**
+   * Gets the OS version.
+   *
+   * @return the OS version
+   */
+  public static String getOsVersion() {
+    return Config.getValue("OSVersion");
+  }
+
+  /**
+   * Gets the device name.
+   *
+   * @return the device name
+   */
+  public static String getDeviceName() {
+    return Config.getValue("DeviceName");
+  }
+
+  /**
+   * Checks if is using mobile browser.
+   *
+   * @return true, if is using mobile browser
+   */
+  public static boolean isUsingMobileBrowser() {
+    String value = Config.getValue("MobileBrowser", "NO");
+
+    if (value.equalsIgnoreCase("YES")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Gets the mobile hub url string.
+   *
+   * @return the mobile hub url string
+   */
+  public static String getMobileHubUrlString() {
+    return Config.getValue("MobileHubUrl");
+  }
+
+  /**
+   * Gets the mobile hub url.
+   *
+   * @return the mobile hub url
+   */
+  public static URL getMobileHubUrl() {
+    return getMobileHubUrl(getMobileHubUrlString());
+  }
+
+  /**
+   * Gets the mobile hub url.
+   *
+   * @param urlString
+   *          the url string
+   * @return the mobile hub url
+   */
+  public static URL getMobileHubUrl(String urlString) {
+    URL url = null;
+    try {
+      url = new URL(urlString);
+    } catch (MalformedURLException e) {
+      e.getStackTrace();
+    }
+
+    return url;
+  }
+
+  /**
+   * Mobile device.
+   *
+   * @return the appium driver
+   */
+  public static AppiumDriver mobileDevice() {
+    return mobileDevice(getMobileDeviceOs());
+  }
+
+  /**
+   * Mobile device.
+   *
+   * @param mobileDeviceOs
+   *          the mobile device OS
+   * @return the appium driver
+   */
+  public static AppiumDriver mobileDevice(String mobileDeviceOs) {
+    AppiumDriver appiumDriver = null;
+    switch (mobileDeviceOs.toUpperCase()) {
+      case "ANDROID":
+
+        appiumDriver = new AndroidDriver(getMobileHubUrl(), getMobileCapabilities());
+        break;
+
+      case "IOS":
+
+        appiumDriver = new IOSDriver(getMobileHubUrl(), getMobileCapabilities());
+        break;
+
+      default:
+
+        throw new IllegalArgumentException(StringProcessor
+            .safeFormatter("Remote browser type %s is not supported", mobileDeviceOs));
+
+    }
+
+    return appiumDriver;
+  }
+
+  /**
+   * Gets the mobile capabilities.
+   *
+   * @return the mobile capabilities
+   */
+  private static DesiredCapabilities getMobileCapabilities() {
+
+    DesiredCapabilities capabilities = null;
+
+    String mobileDeviceOs = getMobileDeviceOs();
+    capabilities = new DesiredCapabilities();
+
+    switch (mobileDeviceOs.toUpperCase()) {
+      case "ANDROID":
+
+        capabilities.setCapability(CapabilityType.PLATFORM, Platform.ANDROID);
+        capabilities.setCapability("appPackage", getBundleId());
+        if (Config.getValue("AppActivity", null) != null) {
+          capabilities.setCapability("appActivity", Config.getValue("AppActivity"));
+        }
+        break;
+
+      case "IOS":
+
+        capabilities.setCapability("bundleId", getBundleId());
+        capabilities.setCapability("udid", getMobileDeviceUdid());
+        break;
+
+      default:
+        throw new IllegalArgumentException(StringProcessor
+            .safeFormatter("Mobile Device OS type %s is not supported", mobileDeviceOs));
+
+    }
+
+    capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, getOsVersion());
+    capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, getMobileDeviceOs());
+
+    capabilities.setCapability(CapabilityType.BROWSER_NAME, getDeviceName());
+    capabilities.setCapability(CapabilityType.VERSION, getOsVersion());
+    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, getDeviceName());
+
+    return capabilities;
+  }
+
+  /**
+   * Sets the timeouts.
+   *
+   * @param driver
+   *          the new timeouts
+   */
+  public static void setTimeouts(AppiumDriver driver) {
+    int timeoutTime = Integer.parseInt(Config.getValue("Timeout", "0"));
+    driver.manage().timeouts().pageLoadTimeout(timeoutTime, null);
+  }
+}
---
+++ Framework/jmaqs-appium/src/main/java/com/magenic/jmaqs/appium/baseAppiumTest/BaseAppiumTest.java
@@ -0,0 +1,109 @@
+/* 
+ * Copyright 2017 (C) Magenic, All rights Reserved
+ */
+
+package com.magenic.maqs.appium.baseAppiumTest;
+
+import com.magenic.jmaqs.baseTest.BaseGenericTest;
+import com.magenic.jmaqs.selenium.baseSeleniumTest.SeleniumWait;
+import com.magenic.jmaqs.utilities.helper.StringProcessor;
+import com.magenic.jmaqs.utilities.logging.MessageType;
+
+import io.appium.java_client.AppiumDriver;
+
+import org.testng.ITestResult;
+
+
+
+/**
+ * Base Appium Test Class.
+ *
+ * @author JasonE
+ */
+public abstract class BaseAppiumTest extends BaseGenericTest {
+
+  /** Thread local storage of AppiumTestObject. */
+  private ThreadLocal<AppiumTestObject> appiumTestObject = new ThreadLocal<AppiumTestObject>();
+
+  /**
+   * Initialize a new instance of the BaseAppiumTest class.
+   */
+  public BaseAppiumTest() {
+
+  }
+
+  /**
+   * Gets the appium driver.
+   *
+   * @return the appium driver
+   */
+  public AppiumDriver getAppiumDriver() {
+    return this.appiumTestObject.get().getAppiumDriver();
+  }
+
+  /**
+   * Gets the appium wait.
+   *
+   * @return the appium wait
+   */
+  public SeleniumWait getAppiumWait() {
+    return this.appiumTestObject.get().getAppiumWait();
+  }
+
+  /**
+   * Gets the appium test object.
+   *
+   * @return the appium test object
+   */
+  public AppiumTestObject getAppiumTestObject() {
+    return this.appiumTestObject.get();
+  }
+
+  /**
+   * Overload function for doing post setup logging.
+   */
+  protected void postSetupLogging() {
+    try {
+
+      AppiumDriver driver = AppiumConfig.mobileDevice();
+      AppiumWait wait = new AppiumWait(driver);
+
+      appiumTestObject.set(new AppiumTestObject(driver, wait, this.getFullyQualifiedTestClassName(),
+          this.getLogger()));
+      this.getLogger().logMessage(MessageType.INFORMATION, "Loaded driver: %s",
+          AppiumConfig.getMobileDeviceOs());
+      // }
+    } catch (Exception e) {
+      this.getLogger().logMessage(MessageType.ERROR, "Failed to start driver because: %s",
+          e.getMessage());
+      System.out.println(
+          StringProcessor.safeFormatter("Browser type %s is not supported", e.getMessage()));
+    }
+  }
+
+  /**
+   * Steps to do before logging teardown results.
+   *
+   * @param resultType
+   *          The test result
+   */
+  protected void beforeLoggingTeardown(ITestResult resultType) {
+    try {
+      // TODO add screen capture once SeleniumUtilities has been created
+      /*
+       * if (this.Log is FileLogger && resultType != TestResultType.PASS &&
+       * this.LoggingEnabledSetting != LoggingEnabled.NO) {
+       * SeleniumUtilities.CaptureScreenshot(this.WebDriver, this.Log); }
+       */
+    } catch (Exception e) {
+      this.tryToLog(MessageType.WARNING, "Failed to get screen shot because: %s", e.getMessage());
+    }
+    this.tryToLog(MessageType.INFORMATION, "Close");
+
+    try {
+      this.appiumTestObject.get().appiumDriver.quit();
+    } catch (Exception e) {
+      this.tryToLog(MessageType.WARNING, "Failed to quit because: %s", e.getMessage());
+    }
+  }
+}
