8202bbe965024383f20c68e00b43752c59df326a
ColeG <coleg@magenic.com>
2019-03-04 10:41:34 -0600

    add generic wait and unit tests

---
+++ Framework/jmaqs-utilities/src/test/java/com/magenic/jmaqs/utilities/unitTests/GenericWaitUnitTest.java
@@ -0,0 +1,247 @@
+package com.magenic.jmaqs.utilities.unitTests;
+
+import com.magenic.jmaqs.utilities.helper.GenericWait;
+import com.magenic.jmaqs.utilities.helper.TimeoutException;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Unit test for the GenericWait class.
+ */
+public class GenericWaitUnitTest {
+  /**
+   * Constant test string.
+   */
+  private String teststring = "Test String";
+
+  /**
+   * Test override retry time.
+   */
+  private static long testretry = 200;
+
+  /**
+   * Test override time out time.
+   */
+  private static long testtimeout = 1000;
+
+  /**
+   * Counter for unit tests.
+   */
+  private static int number = 0;
+
+  /**
+   * Test wait until with no parameters works when the wait function returns true.
+   */
+  @Test
+  public void passNoParamUntilTest() {
+    // have to use an array because the iterator needs to be mutable
+    int[] loop = { 0 };
+    Assert.assertTrue(GenericWait.waitUntil(() ->  loop[0]++ > 3), "Failed no parameter test");
+  }
+
+  /**
+   * Test wait until with an array of parameters works when the wait function returns true.
+   */
+  @Test
+  public void passObjectArrayUntilTest() {
+    ArrayList<Object> objects = new ArrayList<>();
+    objects.add("one");
+    objects.add(new HashMap<Integer, UUID>());
+
+    Assert.assertTrue(GenericWait.waitUntil(this::isTwoParameters, objects.toArray()), "Failed parameter array test");
+  }
+
+  /**
+   * Test wait for with an array of parameters works when the wait function returns true.
+   */
+  @Test
+  public void passObjectArrayForTest() {
+    ArrayList<Object> objects = new ArrayList<>();
+    objects.add("one");
+    objects.add(new HashMap<Integer, UUID>());
+
+    try {
+      GenericWait.waitFor(this::isTwoParameters, objects.toArray());
+    } catch (Exception e) {
+      Assert.fail("waitFor generic object test failed with exception: " + e);
+    }
+  }
+
+  /**
+   * Test wait until with a single parameter works when the wait function returns false.
+   */
+  @Test
+  public void failStringUntilTest() {
+    Assert.assertFalse(GenericWait.waitUntil(this::isParamTestString, "Bad"), "Failed single parameter test");
+  }
+
+  /**
+   * Test wait until with a parameter array works when the wait function returns false.
+   */
+  @Test
+  public void failObjectArrayUntilTest() {
+    ArrayList<Object> objects = new ArrayList<>();
+    Assert.assertFalse(GenericWait.waitUntil(this::isTwoParameters, objects.toArray()), "Failed parameter array test");
+  }
+
+  /**
+   * Test wait until with a parameter array works when the wait function returns false.
+   */
+  @Test(expectedExceptions = NotImplementedException.class)
+  public void throwExceptionWithoutParamTest() throws Exception {
+    GenericWait.waitForTrue(this::throwError);
+  }
+
+  /**
+   * Test wait for with no parameters returns the a timeout exception.
+   */
+  @Test(expectedExceptions = TimeoutException.class)
+  public void throwTimeoutExceptionWithoutParamTest() throws Exception {
+    GenericWait.waitForTrue(this::isParamTest);
+  }
+
+  /**
+   * Test wait for with a parameter returns the function exception when the check times out.
+   */
+  @Test(expectedExceptions = RuntimeException.class)
+  public void throwExceptionWithParamTest() throws Exception {
+    GenericWait.waitForTrue(this::throwError, teststring);
+  }
+
+  /**
+   * Test wait for with parameters returns the a timeout exception.
+   */
+  @Test(expectedExceptions = TimeoutException.class)
+  public void throwTimeoutExceptionWithParamTest() throws Exception {
+    GenericWait.waitForTrue(this::isTwoParameters, (new ArrayList<Object>()).toArray());
+  }
+
+  /**
+   * Test wait without parameters returns function exception.
+   */
+  @Test(expectedExceptions = NotImplementedException.class)
+  public void throwExceptionWithoutParamWithCustomTimesTest() throws Exception {
+    GenericWait.wait(this::throwError, testretry, testtimeout, true);
+  }
+
+  /**
+   * Test wait with parameters returns function exception.
+   */
+  @Test(expectedExceptions = RuntimeException.class)
+  public void throwExceptionWithParamWithCustomTimesTest() throws Exception {
+    GenericWait.wait(this::throwError, testretry, testtimeout, true, "Anything");
+  }
+
+  /**
+   * Verify custom timeout without parameters works.
+   */
+  @Test
+  public void customTimeoutWithoutParamTest() {
+    try {
+      long max = testtimeout + testretry + testretry;
+      LocalDateTime start = LocalDateTime.now();
+      GenericWait.wait(this::isParamTest, testretry, testtimeout, false);
+      long duration = ChronoUnit.MILLIS.between(start, LocalDateTime.now());
+      Assert.assertTrue(duration < max, "The max wait time should be no more than " + max + " but was " + duration);
+    } catch (Exception e) {
+      Assert.fail("wait threw unexpected exception: " + e);
+    }
+  }
+
+  /**
+   * Verify custom timeout with parameters works.
+   */
+  @Test
+  public void customTimeoutWithParamTest() {
+    try {
+      long max = testtimeout + testretry + testretry;
+      LocalDateTime start = LocalDateTime.now();
+      GenericWait.wait(this::isParamTestString, testretry, testtimeout, false, "bad");
+      long duration = ChronoUnit.MILLIS.between(start, LocalDateTime.now());
+      Assert.assertTrue(duration < max, "The max wait time should be no more than " + max + " but was " + duration);
+    } catch (Exception e) {
+      Assert.fail("wait threw unexpected exception: " + e);
+    }
+  }
+
+  /**
+   * Verify that Wait with input parameter throws expected exception.
+   */
+  @Test(expectedExceptions = TimeoutException.class)
+  public void waitForFunctionWithInputExceptionThrown() throws InterruptedException, TimeoutException {
+    GenericWait.wait(this::throwError, testretry, testtimeout, "input");
+  }
+
+  /**
+   * Verify that WaitFor returns the correct value of its called function.
+   */
+  @Test
+  public void waitForTest() {
+    try {
+      Assert.assertFalse(GenericWait.waitFor(this::isParamTest));
+    } catch (Exception e) {
+      Assert.fail("waitFor threw unexpected exception: " + e);
+    }
+  }
+
+  /**
+   * Verify that Wait without input parameter throws expected exception.
+   */
+  @Test(expectedExceptions = TimeoutException.class)
+  public void waitForFunctionWithoutInputExceptionThrown() throws InterruptedException, TimeoutException {
+    GenericWait.wait(this::throwError, testretry, testtimeout);
+  }
+
+  /**
+   * Test function that always returns false.
+   * @return Always returns false
+   */
+  private boolean isParamTest() {
+    return false;
+  }
+
+  /**
+   * Test function that checks if the test string passed in is the same as the constant test string.
+   * @param testString
+   *          The test string
+   * @return True if the constant and passed in test strings match
+   */
+  private boolean isParamTestString(String testString) {
+    return testString.equals(this.teststring + number++);
+  }
+
+  /**
+   * Test function that checks if the object array passed in is in the form expected.
+   * @param parameters
+   *          Object array
+   * @return True if the array is in the form expected
+   */
+  private boolean isTwoParameters(Object[] parameters) {
+    return (parameters.length == 2 && (parameters[0] instanceof String) && (parameters[1] instanceof HashMap));
+  }
+
+  /**
+   * Test function that always throws a not implemented exception.
+   * @return Always throws an exception
+   */
+  private boolean throwError() {
+    throw new NotImplementedException();
+  }
+
+  /**
+   * Test function that always throws a runtime exception.
+   * @param testString
+   *          Test string
+   * @return Always throws an exception
+   */
+  private boolean throwError(String testString) {
+    throw new RuntimeException();
+  }
+}
