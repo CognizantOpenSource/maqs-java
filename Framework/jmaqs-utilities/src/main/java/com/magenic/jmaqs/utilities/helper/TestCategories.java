7d82e1f049bb1e25aa0cb20cf60637c721db8872
Dylan Salmi <DylanS@magenic.com>
2019-05-22 17:04:13 -0500

    remove IDE specific files update utilities/helper

---
+++ Framework/jmaqs-utilities/src/main/java/com/magenic/jmaqs/utilities/helper/TestCategories.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019 (C) Magenic, All rights Reserved
+ */
+
+package com.magenic.jmaqs.utilities.helper;
+
+/**
+ * Test category type
+ */
+public class TestCategories {
+
+    /**
+     * String for email unit test category
+     */
+    public static final String Email = "Email Unit Tests";
+
+    /**
+     * String for database unit test category
+     */
+    public static final String Database = "Database Unit Tests";
+
+    /**
+     * String for Selenium unit test category
+     */
+    public static final String Selenium = "Selenium Unit Tests";
+
+    /**
+     * String for utilities unit test category
+     */
+    public static final String Utilities = "Utilities Unit Tests";
+
+    /**
+     * String for web service unit test category
+     */
+    public static final String WebService = "Web Service Unit Tests";
+
+    /**
+     * String for base framework unit test category
+     */
+    public static final String Framework = "Base Framework Unit Tests";
+
+    /**
+     * String for media type test category
+     */
+    public static final String MediaType = "Media Type Unit Tests";
+
+    /**
+     * String for MongoDB type test category
+     */
+    public static final String MongoDB = "MongoDB Unit Tests";
+
+    /**
+     * String for Appium unit test category
+     */
+    public static final String Appium = "Appium Unit Tests";
+}
