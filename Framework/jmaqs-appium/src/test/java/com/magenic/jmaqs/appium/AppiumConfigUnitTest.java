b826ce0c26a71ea62df310de2a125f215f136c61
Jason Edstrom <JasonE@magenic.com>
2019-09-04 15:51:45 -0500

    Last

--- Framework/jmaqs-appium/src/test/java/com/magenic/jmaqs/appium/AppiumConfigUnitTest.java
+++ Framework/jmaqs-appium/src/test/java/com/magenic/jmaqs/appium/AppiumConfigUnitTest.java
@@ -9,7 +9,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
-public class AppiumConfigUnitTests {
+public class AppiumConfigUnitTest {
 
     @Test
     public void testGetMobileDeviceUDID() throws Exception {
