7f806af18a96391b89c246b64fffa71f7b6e6e22
Derrick Mugwaneza <DerrickM@magenic.com>
2019-09-05 15:56:28 -0500

    Added methods to meet MAQS parity

--- Framework/jmaqs-selenium/src/main/java/com/magenic/jmaqs/selenium/SeleniumTestObject.java
+++ Framework/jmaqs-selenium/src/main/java/com/magenic/jmaqs/selenium/SeleniumTestObject.java
@@ -25,7 +25,7 @@ public class SeleniumTestObject extends BaseTestObject {
   protected SeleniumWait seleniumWait;
 
   /**
-   * Initializes a new instance of the SeleniunTestObject.
+   * Initializes a new instance of the SeleniumTestObject.
    * 
    * @param driver
    *          The WebDriver Object
@@ -40,6 +40,20 @@ public class SeleniumTestObject extends BaseTestObject {
     super(logger, fullyQualifiedTestName);
     this.webDriver = driver;
     this.seleniumWait = wait;
+  }
+
+  /**
+   *
+   * @param getDriver
+   * @param logger
+   * @param fullyQualifiedTestName+
+   */
+  // TODO: Review with Jason and wait for SeleniumDriverManager and softAssert completion
+  // public SeleniumTestObject(Func<IWebDriver> getDriver, Logger logger, string fullyQualifiedTestName) : base(logger, fullyQualifiedTestName)
+  public SeleniumTestObject(WebDriver getDriver, Logger logger, String fullyQualifiedTestName) {
+    super(logger, fullyQualifiedTestName);
+    this.managerStore.Add(typeof(SeleniumDriverManager).FullName, new SeleniumDriverManager(getDriver, this));
+    this.softAssert = new SeleniumSoftAssert(this);
   }
 
   /**
@@ -70,6 +84,14 @@ public class SeleniumTestObject extends BaseTestObject {
     return this.webDriver;
   }
 
+
+  /**
+   * Gets the Selenium driver manager
+   */
+  public SeleniumDriverManager getWebManager
+  {
+      return this.ManagerStore[typeof(SeleniumDriverManager).FullName] instanceof SeleniumDriverManager;
+  }
   /**
    * Set the WebDriver for the SeleniumTestObject.
    * 
@@ -79,4 +101,24 @@ public class SeleniumTestObject extends BaseTestObject {
   public void setWebDriver(WebDriver driver) {
     this.webDriver = driver;
   }
+
+  /**
+   * Override the Selenium web driver
+   * @param webDriver New web driver
+   */
+  public void OverrideWebDriver(WebDriver webDriver)
+  {
+    this.OverrideDriverManager(typeof(SeleniumDriverManager).FullName, new SeleniumDriverManager(() => webDriver, this));
+  }
+
+  /**
+   * Override the function for creating a Selenium web driver
+   * @param getDriver Function for creating a web driver
+   */
+  // public void OverrideWebDriver(Func<WebDriver> getDriver)
+  public void OverrideWebDriver(WebDriver getDriver)
+  {
+    this.OverrideDriverManager(typeof(SeleniumDriverManager).FullName, new SeleniumDriverManager(getDriver, this));
+  }
+
 }
