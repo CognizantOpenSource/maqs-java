6ff681bcfa3fe805a19d18af582e997f260cb54c
Jason Edstrom <JasonE@magenic.com>
2019-09-12 09:36:45 -0500

    Initial Stub out for appium driver factory

--- Framework/jmaqs-appium/src/main/java/com/magenic/jmaqs/appium/AppiumDriverFactory.java
+++ Framework/jmaqs-appium/src/main/java/com/magenic/jmaqs/appium/AppiumDriverFactory.java
@@ -1,5 +1,46 @@
 package com.magenic.jmaqs.appium;
 
+import io.appium.java_client.AppiumDriver;
+import java.net.URI;
+import java.time.Duration;
+import java.util.Map;
+import java.util.function.Function;
+import org.openqa.selenium.WebElement;
+
 public class AppiumDriverFactory {
-  
+
+  public static AppiumDriver<WebElement> getDefaultMobileDriver() {
+    return getDefaultMobileDriver(AppiumConfig.getDeviceType());
+  }
+
+  public static AppiumDriver<WebElement> getDefaultMobileDriver(PlatformType deviceType) {
+
+  }
+
+  public static AppiumDriver<WebElement> createDriver(Function<> createFunction) {
+
+  }
+
+  public static AppiumDriver<WebElement> getAndroidDriver(URI mobileHub, AppiumOptions options,
+      Duration timeout) {
+
+  }
+
+  public static AppiumDriver<WebElement> getIOSDriver(URI mobileHub, AppiumOptions options,
+      Duration timeout) {
+
+  }
+
+  public static AppiumOptions getDefaultMobileOptions() {
+
+  }
+
+  public static AppiumOptions getDefaultMobileOptions(Map<String, Object> capabilities) {
+
+  }
+
+  public static AppiumDriver<WebElement> getWindowsDriver(URI mobileHub, AppiumOptions options,
+      Duration timeout) {
+
+  }
 }
