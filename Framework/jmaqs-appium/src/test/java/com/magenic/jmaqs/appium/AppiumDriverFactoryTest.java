d7f0cecf82b71b98f2385848f8a4d6cfbd3964ee
Jason Edstrom <JasonE@magenic.com>
2019-09-26 16:31:30 -0500

    Test Class created for factory

---
+++ Framework/jmaqs-appium/src/test/java/com/magenic/jmaqs/appium/AppiumDriverFactoryTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2019 (C) Magenic, All rights Reserved
+ */
+
+package com.magenic.jmaqs.appium;
+
+import org.testng.annotations.Test;
+
+public class AppiumDriverFactoryTest {
+
+  private static final String MESSAGE = "Test method not implemented yet.";
+
+  @Test
+  public void testGetDefaultMobileDriver() {
+    throw new UnsupportedOperationException(MESSAGE);
+  }
+
+  @Test
+  public void testTestGetDefaultMobileDriver() {
+
+    throw new UnsupportedOperationException(MESSAGE);
+  }
+
+  @Test
+  public void testGetDefaultMobileOptions() {
+    throw new UnsupportedOperationException(MESSAGE);
+  }
+
+  @Test
+  public void testTestGetDefaultMobileOptions() {
+    throw new UnsupportedOperationException(MESSAGE);
+  }
+
+  @Test
+  public void testGetAndroidDriver() {
+    throw new UnsupportedOperationException(MESSAGE);
+  }
+
+  @Test
+  public void testGetIOSDriver() {
+    throw new UnsupportedOperationException(MESSAGE);
+  }
+
+  @Test
+  public void testGetWindowsDriver() {
+    throw new UnsupportedOperationException(MESSAGE);
+  }
+
+  @Test
+  public void testCreateDriver() {
+    throw new UnsupportedOperationException(MESSAGE);
+  }
+}
