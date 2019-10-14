7ff9ec2fe9b8f2072a83bb6a7a0bb1247a666f83
Jon Reding <jonr@magenic.com>
2019-10-14 10:59:59 -0500

    Merge branch 'master' of C:\Users\jonr\JMAQS with conflicts.

--- Framework/jmaqs-base/src/main/java/com/magenic/jmaqs/base/BaseExtendableTest.java
+++ Framework/jmaqs-base/src/main/java/com/magenic/jmaqs/base/BaseExtendableTest.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2019 (C) Magenic, All rights Reserved
  */
@@ -11,10 +12,10 @@ import org.testng.annotations.BeforeMethod;
 
 /**
  * The type Base extendable test.
- *
  * @param <T> the type parameter
  */
-public abstract class BaseExtendableTest<T extends BaseTestObject> extends BaseTest {
+public abstract class BaseExtendableTest<T extends BaseTestObject>
+        extends BaseTest {
 
   /**
    * Instantiates a new Base extendable test.
@@ -22,25 +23,45 @@ public abstract class BaseExtendableTest<T extends BaseTestObject> extends BaseT
   protected BaseExtendableTest() {
   }
 
+  /**
+   * gets the test object.
+   * @return the test object.
+   */
+  @Override
+  @SuppressWarnings("unchecked")
   public T getTestObject() {
     return (T) super.getTestObject();
   }
 
+  /**
+   * sets the test object.
+   * @param baseTestObject The Base Test Object to use
+   */
   @Override
-  public void setTestObject(BaseTestObject baseTestObject) {
-
-    if (this.baseTestObjects.putIfAbsent(this.getFullyQualifiedTestClassName(), baseTestObject)
-        == null) {
-      this.baseTestObjects.replace(this.getFullyQualifiedTestClassName(), baseTestObject);
+  public void setTestObject(final BaseTestObject baseTestObject) {
+    if (this.baseTestObjects.putIfAbsent(this.getFullyQualifiedTestClassName(),
+            baseTestObject) == null) {
+      this.baseTestObjects.replace(this.getFullyQualifiedTestClassName(),
+              baseTestObject);
     }
   }
 
+  /**
+   * sets up the Base Extendable Test.
+   * @param method      The initial executing Method object
+   * @param testContext The initial executing Test Context object
+   * @throws Exception throws error if set up fails.
+   */
   @BeforeMethod
   @Override
-  public void setup(Method method, ITestContext testContext) throws Exception {
+  public void setup(final Method method, final ITestContext testContext)
+          throws Exception {
     super.setup(method, testContext);
   }
 
+  /**
+   * creates the new test object.
+   */
+  @Override
   protected abstract void createNewTestObject();
 }
-
