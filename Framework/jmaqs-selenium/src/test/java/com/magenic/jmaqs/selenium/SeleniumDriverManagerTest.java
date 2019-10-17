4cab1db361167b43248f23cf42359f13c9cca1ca
Jason Edstrom <JasonE@magenic.com>
2019-10-17 14:55:04 -0500

    JMAQS-42 SeleniumDriverManager - Unit test file added

---
+++ Framework/jmaqs-selenium/src/test/java/com/magenic/jmaqs/selenium/SeleniumDriverManagerTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019 (C) Magenic, All rights Reserved
+ */
+
+package com.magenic.jmaqs.selenium;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SeleniumDriverManagerTest {
+
+  @Test
+  public void testClose() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  @Test
+  public void testGetWebDriver() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  @Test
+  public void testLogVerbose() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+}
