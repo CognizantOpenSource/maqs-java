a7c20276b0c83dd12b07e5f8f6fc3932496f16fa
Jason Edstrom <JasonE@magenic.com>
2019-10-21 17:06:28 -0500

    JMAQS #35 - AppiumTestObject - 0h 33m - Created Unit test class
    Module [JMAQS]:
      + AppiumTestObjectTest.java

---
+++ Framework/jmaqs-appium/src/test/java/com/magenic/jmaqs/appium/AppiumTestObjectTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 (C) Magenic, All rights Reserved
+ */
+
+package com.magenic.jmaqs.appium;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * The type Appium test object test.
+ */
+public class AppiumTestObjectTest {
+
+  /**
+   * Test get appium driver.
+   */
+  @Test
+  public void testGetAppiumDriver() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  /**
+   * Test get appium manager.
+   */
+  @Test
+  public void testGetAppiumManager() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  /**
+   * Test set appium driver.
+   */
+  @Test
+  public void testSetAppiumDriver() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  /**
+   * Test test set appium driver.
+   */
+  @Test
+  public void testTestSetAppiumDriver() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+}
