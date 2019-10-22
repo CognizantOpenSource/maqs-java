362acf071118d8ddd642158025f9d3332ea4496c
Jason Edstrom <JasonE@magenic.com>
2019-10-21 23:23:17 -0500

    JMAQS #40 - BaseAppiumTest - 0h 50m - Unit Test File created
    Module [JMAQS]:
      + BaseAppiumTestTest.java

---
+++ Framework/jmaqs-appium/src/test/java/com/magenic/jmaqs/appium/BaseAppiumTestTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2019 (C) Magenic, All rights Reserved
+ */
+
+package com.magenic.jmaqs.appium;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class BaseAppiumTestTest {
+
+  @Test
+  public void testGetAppiumDriver() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  @Test
+  public void testSetAppiumDriver() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  @Test
+  public void testGetAppiumTestObject() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  @Test
+  public void testGetMobileDriver() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  @Test
+  public void testBeforeLoggingTeardown() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+}
