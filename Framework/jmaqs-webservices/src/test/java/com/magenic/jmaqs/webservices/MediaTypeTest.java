3ee6515f6be92aec4167ba3045a9e9ac228e3882
Jason Edstrom <JasonE@magenic.com>
2019-10-24 08:09:27 -0500

    JMAQS #53 - MediaType - 0h 18m - Enum values and unit tests for logic completed.
    Module [JMAQS]:
      + MediaType.java
      + MediaTypeTest.java

---
+++ Framework/jmaqs-webservices/src/test/java/com/magenic/jmaqs/webservices/MediaTypeTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019 (C) Magenic, All rights Reserved
+ */
+
+package com.magenic.jmaqs.webservices;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MediaTypeTest {
+
+  @Test
+  public void testTestToStringAppJson() {
+    Assert.assertEquals(MediaType.APP_JSON.toString(), MediaType.APP_JSON.getMediaTypeString());
+  }
+
+  @Test
+  public void testTestToStringAppXml() {
+    Assert.assertEquals(MediaType.APP_XML.toString(), MediaType.APP_XML.getMediaTypeString());
+  }
+
+  @Test
+  public void testTestToStringPlainText() {
+    Assert.assertEquals(MediaType.PLAIN_TEXT.toString(), MediaType.PLAIN_TEXT.getMediaTypeString());
+  }
+
+  @Test
+  public void testTestToStringImagePng() {
+    Assert.assertEquals(MediaType.IMAGE_PNG.toString(), MediaType.IMAGE_PNG.getMediaTypeString());
+  }
+
+}
