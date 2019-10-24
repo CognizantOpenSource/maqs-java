b600be19b1972d81b45eaac8992832b72d37c957
Jason Edstrom <JasonE@magenic.com>
2019-10-24 14:32:15 -0500

    JMAQS #65 - SeleniumTestObject - 1h 15m - Constructor unit tests working, stubs for remaining tests.

--- Framework/jmaqs-selenium/src/test/java/com/magenic/jmaqs/selenium/SeleniumTestObjectTest.java
+++ Framework/jmaqs-selenium/src/test/java/com/magenic/jmaqs/selenium/SeleniumTestObjectTest.java
@@ -6,7 +6,11 @@ package com.magenic.jmaqs.selenium;
 
 import com.magenic.jmaqs.utilities.logging.ConsoleLogger;
 import java.lang.reflect.Method;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
@@ -30,4 +34,58 @@ public class SeleniumTestObjectTest {
 
   }
 
+  /**
+   * Test appium test object creation with driver.
+   */
+  @Test
+  public void testSeleniumTestObjectCreationWithDriver() {
+
+    WebDriver defaultBrowser = null;
+    try {
+      defaultBrowser = WebDriverFactory.getDefaultBrowser();
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail("Driver creation failed. ");
+    }
+    SeleniumTestObject testObject = new SeleniumTestObject(defaultBrowser, consoleLogger, testName);
+    Assert.assertNotNull(testObject, "Checking that selenium test object via driver is not null");
+  }
+
+  /**
+   * Test appium test object creation with supplier.
+   */
+  @Test
+  public void testSeleniumTestObjectCreationWithSupplier() {
+
+    try (SeleniumTestObject testObject = new SeleniumTestObject(() -> {
+      try {
+        return WebDriverFactory.getDefaultBrowser();
+      } catch (Exception e) {
+        Assert.fail("Driver creation failed.");
+      }
+      return null;
+    }, consoleLogger, testName)) {
+      Assert.assertNotNull(testObject, "Checking that appium test object via supplier is not null");
+    }
+  }
+
+  @Test
+  public void testGetWebDriver() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  @Test
+  public void testGetWebManager() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  @Test
+  public void testSetWebDriver() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  @Test
+  public void testSetWebDriverSupplier() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
 }
