c6aabd8ed31426659fe25d2d53a82d9c6d7fe48f
Jason Edstrom <JasonE@magenic.com>
2019-10-30 13:44:22 -0500

    JMAQS #47 - BaseSeleniumTest - 4h 25m - Generic test works better

--- Framework/jmaqs-selenium/src/test/java/com/magenic/jmaqs/selenium/SeleniumTestObjectTest.java
+++ Framework/jmaqs-selenium/src/test/java/com/magenic/jmaqs/selenium/SeleniumTestObjectTest.java
@@ -4,7 +4,7 @@
 
 package com.magenic.jmaqs.selenium;
 
-import com.magenic.jmaqs.base.BaseTest;
+import com.magenic.jmaqs.base.BaseGenericTest;
 import com.magenic.jmaqs.utilities.helper.TestCategories;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 /**
  * The type Selenium test object test.
  */
-public class SeleniumTestObjectTest extends BaseTest {
+public class SeleniumTestObjectTest extends BaseGenericTest {
 
   /**
    * Test appium test object creation with driver.
