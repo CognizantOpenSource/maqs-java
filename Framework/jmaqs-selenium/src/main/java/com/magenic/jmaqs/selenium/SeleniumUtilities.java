c0f696e5407853fd59f8033a2169a21dc2da6c52
Jason Edstrom <JasonE@magenic.com>
2019-10-30 15:42:49 -0500

    JMAQS #46 - SeleniumUtilities - 1h 16m - Utilities initial implementation

--- Framework/jmaqs-selenium/src/main/java/com/magenic/jmaqs/selenium/SeleniumUtilities.java
+++ Framework/jmaqs-selenium/src/main/java/com/magenic/jmaqs/selenium/SeleniumUtilities.java
@@ -4,16 +4,25 @@
 
 package com.magenic.jmaqs.selenium;
 
+import com.magenic.jmaqs.utilities.helper.StringProcessor;
 import com.magenic.jmaqs.utilities.logging.FileLogger;
 import com.magenic.jmaqs.utilities.logging.Logger;
 import com.magenic.jmaqs.utilities.logging.LoggingConfig;
 import com.magenic.jmaqs.utilities.logging.MessageType;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.text.MessageFormat;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.openqa.selenium.OutputType;
@@ -21,6 +30,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WrapsDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 /**
@@ -29,80 +39,192 @@ import org.openqa.selenium.support.events.EventFiringWebDriver;
 public class SeleniumUtilities {
 
   /**
+   * Default Date Time Format for appending to files.
+   */
+  private static final String DEFAULT_DATE_TIME_FORMAT = "uuuu-MM-dd-HH-mm-ss-SSSS";
+
+  private SeleniumUtilities() {
+  }
+
+  /**
+   * Capture screenshot boolean.
+   *
+   * @param webDriver  the web driver
+   * @param testObject the test object
+   * @return the boolean
+   */
+  public static boolean captureScreenshot(WebDriver webDriver, SeleniumTestObject testObject) {
+    return captureScreenshot(webDriver, testObject, "");
+  }
+
+  /**
    * To capture a screenshot during execution.
-   * 
-   * @param webDriver
-   *          The WebDriver
-   * @param log
-   *          The logger being used
-   * @param appendName
-   *          Appends a name to the end of a filename
+   *
+   * @param webDriver  The WebDriver
+   * @param testObject the test object
+   * @param appendName Appends a name to the end of a filename
    * @return Boolean if the save of the image was successful
    */
-  public static String captureScreenshot(WebDriver webDriver, 
-                                          Logger log, 
-                                          String appendName) {
+  public static boolean captureScreenshot(WebDriver webDriver, SeleniumTestObject testObject,
+      String appendName) {
     try {
-      String path;
-
-      // Check if we are using a file logger
-      if (!(log instanceof FileLogger)) {
-        // Since this is not a file logger we will need to use a generic file name
-        path =  captureScreenshot(webDriver, LoggingConfig.getLogDirectory(),"ScreenCap" + appendName);
-        
-      } else {
-        // Calculate the file name
-        String fullpath = ((FileLogger) log).getFilePath();
-        String directory = new File(fullpath).getParent();
-        String fileNameWithoutExtension = FilenameUtils.getBaseName(fullpath);
-
-        path =  captureScreenshot(webDriver, directory, fileNameWithoutExtension);
+      // Check if we are using a file logger. If not, return false.
+      if (!(testObject.getLog() instanceof FileLogger)) {
+        return false;
       }
 
-      log.logMessage(MessageType.INFORMATION, "Screenshot saved: " + path);
-      return path;
+      // Calculate the file name with Date Time Stamp
+      String directory = ((FileLogger) testObject.getLog()).getDirectory();
+      String fileNameWithoutExtension = StringProcessor
+          .safeFormatter("%s - %s%s", testObject.getFullyQualifiedTestName(),
+              DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT, Locale.getDefault())
+                  .format(LocalDateTime.now(Clock.systemUTC())), appendName);
+      captureScreenshot(webDriver, testObject, directory, fileNameWithoutExtension);
+
+      testObject.getLog().logMessage(MessageType.INFORMATION, "Screenshot saved.");
+      return true;
     } catch (Exception exception) {
-      log.logMessage(MessageType.ERROR, "Screenshot error: %s", exception.toString());
-      return "";
+      testObject.getLog().logMessage(MessageType.ERROR,
+          String.format("Screenshot error: %s", exception.getMessage()));
+      return false;
     }
   }
 
   /**
    * To capture a screenshot during execution.
-   * 
-   * @param webDriver
-   *          The WebDriver
-   * @param directory
-   *          The directory file path
-   * @param fileNameWithoutExtension
-   *          Filename without extension
+   *
+   * @param webDriver                The WebDriver
+   * @param testObject               the test object
+   * @param directory                The directory file path
+   * @param fileNameWithoutExtension Filename without extension
    * @return Path to the log file
-   * @throws IOException
-   *           There was a problem creating the screen shot
+   * @throws IOException There was a problem creating the screen shot
    */
-  public static String captureScreenshot(WebDriver webDriver,
-                                         String directory, 
-                                         String fileNameWithoutExtension) throws IOException {
-    
-    File scrFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+  public static String captureScreenshot(WebDriver webDriver, SeleniumTestObject testObject,
+      String directory, String fileNameWithoutExtension) {
+    File tempFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+    String path = Paths.get(directory, fileNameWithoutExtension + ".png").normalize().toString();
 
-    checkDirectory(directory);
+    // Make sure the directory exists
+    try {
+      Path tempPath = new File(directory).toPath();
+      if (!tempPath.toFile().isDirectory()) {
+        Files.createDirectories(tempPath);
+      }
+    } catch (IOException exception) {
+      testObject.getLog()
+          .logMessage(MessageType.ERROR, "Failed to create directories: " + exception.getMessage());
+    }
+
+    // Try to copy the temporary file to desired file path
+    try {
+      Files.copy(tempFile.toPath(), new File(path).toPath(), StandardCopyOption.COPY_ATTRIBUTES);
+    } catch (IOException exception) {
+      testObject.getLog().logMessage(MessageType.ERROR,
+          String.format("Screenshot error: %s", exception.getMessage()));
+    }
+
+    testObject.addAssociatedFile(path);
+    return path;
+  }
+
+  /**
+   * To capture a page source during execution.
+   * Default parameter appendName of empty string.
+   *
+   * @param webDriver  the web driver
+   * @param testObject The Appium Test Object
+   * @return True if saving page source is successful, otherwise false
+   */
+  public static boolean savePageSource(WebDriver webDriver, SeleniumTestObject testObject) {
+    return savePageSource(webDriver, testObject, "");
+  }
+
+  /**
+   * To capture a page source during execution.
+   *
+   * @param webDriver  the web driver
+   * @param testObject The Appium Test Object
+   * @param appendName Appends a name to the end of a filename
+   * @return True if saving page source is successful, otherwise false
+   */
+  public static boolean savePageSource(WebDriver webDriver, SeleniumTestObject testObject,
+      String appendName) {
+    try {
+      String path = "";
+
+      // Check if we are using a file logger.
+      if (!(testObject.getLog() instanceof FileLogger)) {
+        // Since this is not a file logger we will need to use a generic file name
+        path = savePageSource(webDriver, testObject, LoggingConfig.getLogDirectory(),
+            StringProcessor.safeFormatter("%s - %s%s", "PageSource",
+                DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT, Locale.getDefault())
+                    .format(LocalDateTime.now(Clock.systemUTC())), appendName));
+      } else {
+        // Calculate the file name
+        String directory = ((FileLogger) testObject.getLog()).getDirectory();
+        String fileNameWithoutExtension = StringProcessor
+            .safeFormatter("%s - %s%s", testObject.getFullyQualifiedTestName() + "_PS",
+                DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT, Locale.getDefault())
+                    .format(LocalDateTime.now(Clock.systemUTC())), appendName);
+
+        path = savePageSource(webDriver, testObject, directory, fileNameWithoutExtension);
+      }
+
+      testObject.getLog().logMessage(MessageType.INFORMATION, "Page Source saved: " + path);
+      return true;
+    } catch (Exception exception) {
+      testObject.getLog().logMessage(MessageType.ERROR,
+          String.format("Page Source error: %s", exception.getMessage()));
+      return false;
+    }
+  }
+
+  /**
+   * To capture Page Source during execution.
+   *
+   * @param webDriver                the web driver
+   * @param testObject               The Appium Test Object
+   * @param directory                The directory file path
+   * @param fileNameWithoutExtension File Name Without Extension
+   * @return Path to the log file
+   */
+  public static String savePageSource(WebDriver webDriver, SeleniumTestObject testObject,
+      String directory, String fileNameWithoutExtension) {
+    // Save the current page source into a string
+    String pageSource = webDriver.getPageSource();
+
+    // Make sure the directory exists
+    try {
+      Path path = new File(directory).toPath();
+      if (!path.toFile().isDirectory()) {
+        Files.createDirectories(path);
+      }
+    } catch (IOException exception) {
+      testObject.getLog()
+          .logMessage(MessageType.ERROR, "Failed to create directories: " + exception.getMessage());
+    }
 
     // Calculate the file name
-    String filePath = MessageFormat.format("{0}.{1}",fileNameWithoutExtension, SeleniumConfig.getImageFormat());
-    String fullPath = Paths.get(directory, filePath).toString();
+    String path = Paths.get(directory, fileNameWithoutExtension + ".txt").normalize().toString();
 
-    // Save the screenshot
-    FileUtils.copyFile(scrFile, new File(fullPath));
+    try (FileWriter writer = new FileWriter(path, false)) {
+      writer.write(pageSource);
 
-    return fullPath;
-  }  
-  
+      // Flush File Writer
+      writer.flush();
+    } catch (IOException exception) {
+      testObject.getLog().logMessage(MessageType.ERROR, "Failed to start File Writer.");
+    }
+
+    testObject.addAssociatedFile(path);
+    return path;
+  }
+
   /**
    * Get the web driver from a web element.
-   * 
-   * @param webElement
-   *          The web element
+   *
+   * @param webElement The web element
    * @return The web driver
    */
   public static WebDriver webElementToWebDriver(WebElement webElement) {
@@ -119,6 +241,7 @@ public class SeleniumUtilities {
 
   /**
    * Checks if directory exists, creating one if not.
+   *
    * @param directory The directory path
    */
   private static void checkDirectory(String directory) {
@@ -126,6 +249,19 @@ public class SeleniumUtilities {
     File folder = new File(directory);
     if (!folder.isDirectory()) {
       folder.mkdir();
+    }
+  }
+
+  /**
+   * Make sure the driver is shut down.
+   *
+   * @param webDriver the web driver
+   */
+  public static void killDriver(WebDriver webDriver) {
+    try {
+      webDriver.close();
+    } finally {
+      webDriver.quit();
     }
   }
 }
