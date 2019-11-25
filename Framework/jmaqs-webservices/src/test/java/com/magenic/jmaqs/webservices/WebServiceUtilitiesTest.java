63ab2dae2bcb16c7e6a50c222967302b56132b7b
row49382 <r-wroblewski@bethel.edu>
2019-11-25 01:09:14 -0600

    adding additional tests and a toString method to the Product model

--- Framework/jmaqs-webservices/src/test/java/com/magenic/jmaqs/webservices/WebServiceUtilitiesTest.java
+++ Framework/jmaqs-webservices/src/test/java/com/magenic/jmaqs/webservices/WebServiceUtilitiesTest.java
@@ -1,11 +1,14 @@
 package com.magenic.jmaqs.webservices;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.magenic.jmaqs.webservices.models.Product;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.entity.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.nio.charset.Charset;
 
 public class WebServiceUtilitiesTest extends BaseWebServiceTest {
 
@@ -44,9 +47,34 @@ public class WebServiceUtilitiesTest extends BaseWebServiceTest {
     }
 
     @Test
-    public void testGetXmlAsString() throws Exception {
-        CloseableHttpResponse response = this.getHttpClientWrapper().getContent("/api/XML_JSON/GetProduct/1",
-                ContentType.APPLICATION_XML, true);
+    public void testCreateStringEntityJson() throws JsonProcessingException {
+        Product product = new Product();
+        HttpEntity entity = WebServiceUtilities.createStringEntity(product, ContentType.APPLICATION_JSON);
 
+        Assert.assertNotNull(entity, "string entity wasn't created using content type application/json");
+    }
+
+    @Test
+    public void testCreateStringEntityXml() throws JsonProcessingException {
+        Product product = new Product();
+        HttpEntity entity = WebServiceUtilities.createStringEntity(product, ContentType.APPLICATION_XML);
+
+        Assert.assertNotNull(entity, "string entity wasn't created using content type application/xml");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testCreateStringEntityNotJsonOrXml() throws JsonProcessingException {
+        Product product = new Product();
+        HttpEntity entity = WebServiceUtilities.createStringEntity(product, ContentType.APPLICATION_FORM_URLENCODED);
+
+        Assert.fail("Expected exception of IllegalArgumentException was not caught for content type that did not contain xml or json");
+    }
+
+    @Test
+    public void testCreateStringEntityCustomContentType() {
+        Product product = new Product();
+        HttpEntity entity = WebServiceUtilities.createStringEntity(product.toString(), Charset.defaultCharset(), "application/json");
+
+        Assert.assertNotNull(entity, "Entity was not set correctly with custom charset and mime-type");
     }
 }
