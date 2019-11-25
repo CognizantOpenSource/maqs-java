316353c3f04fc0b788d273ee49395d0716cd3b1f
Jason Edstrom <JasonE@magenic.com>
2019-11-25 10:29:33 -0600

    JMAQS #156 - Database Config - 0h 38m - Added Config class and unit test class

---
+++ Framework/jmaqs-database/src/main/java/com/magenic/jmaqs/database/DatabaseConfig.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2019 (C) Magenic, All rights Reserved
+ */
+
+package com.magenic.jmaqs.database;
+
+import com.magenic.jmaqs.utilities.helper.Config;
+import com.magenic.jmaqs.utilities.helper.ConfigSection;
+
+/**
+ * The type Database config.
+ */
+public class DatabaseConfig {
+
+  /**
+   * The Database section.
+   */
+  private static final ConfigSection DATABASE_SECTION = ConfigSection.DatabaseMaqs;
+
+  /**
+   * Gets connection string.
+   *
+   * @return the connection string
+   */
+  public static String getConnectionString() {
+    return Config.getValueForSection(DATABASE_SECTION, "DatabaseConnectionString");
+  }
+
+  /**
+   * Gets provider type string.
+   *
+   * @return the provider type string
+   */
+  public static String getProviderTypeString() {
+    return Config.getValueForSection(DATABASE_SECTION, "DatabaseProviderType");
+  }
+
+}
---
+++ Framework/jmaqs-database/src/test/java/com/magenic/jmaqs/database/DatabaseConfigUnitTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 (C) Magenic, All rights Reserved
+ */
+
+package com.magenic.jmaqs.database;
+
+
+import org.testng.annotations.Test;
+
+/**
+ * The type Database config unit test.
+ */
+public class DatabaseConfigUnitTest {
+
+  /**
+   * Test get connection string.
+   */
+  @Test
+  public void testGetConnectionString() {
+
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+
+  /**
+   * Test get provider type string.
+   */
+  @Test
+  public void testGetProviderTypeString() {
+    throw new UnsupportedOperationException("Test method not implemented yet.");
+  }
+}
