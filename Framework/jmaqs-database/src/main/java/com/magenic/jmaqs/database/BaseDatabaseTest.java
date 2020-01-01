4e3b95bcc7c14881353e1abbe1be3a4d58fb5628
Jason Edstrom <JasonE@magenic.com>
2020-01-01 14:59:09 -0600

    JMAQS #177 - Database - BaseDatabaseTest - 0h 4m - JavaDoc additions

--- Framework/jmaqs-database/src/main/java/com/magenic/jmaqs/database/BaseDatabaseTest.java
+++ Framework/jmaqs-database/src/main/java/com/magenic/jmaqs/database/BaseDatabaseTest.java
@@ -7,19 +7,37 @@ package com.magenic.jmaqs.database;
 import com.magenic.jmaqs.base.BaseExtendableTest;
 import org.testng.ITestResult;
 
+/**
+ * Base Database Test Class
+ */
 public class BaseDatabaseTest extends BaseExtendableTest<DatabaseTestObject> {
 
   public BaseDatabaseTest() {
   }
 
+  /**
+   * Get Database Driver from TestObject
+   *
+   * @return the databaseDriver (type DatabaseDriver) of this BaseDatabaseTest object.
+   */
   public DatabaseDriver getDatabaseDriver() {
     return this.getTestObject().getDatabaseDriver();
   }
 
+  /**
+   * Method setDatabaseDriver sets the databaseDriver of the test object.
+   *
+   * @param driver the databaseDriver of this BaseDatabaseTest object.
+   */
   public void setDatabaseDriver(DatabaseDriver driver) {
     this.getTestObject().setDatabaseDriver(driver);
   }
 
+  /**
+   * Get a new DatabaseDriver from the ConnectionFactory
+   *
+   * @return the connection (type DatabaseDriver) of this BaseDatabaseTest object.
+   */
   public DatabaseDriver getConnection() {
     return ConnectionFactory.getOpenConnection();
   }
@@ -31,8 +49,7 @@ public class BaseDatabaseTest extends BaseExtendableTest<DatabaseTestObject> {
 
   @Override
   protected void createNewTestObject() {
-    this.setTestObject(new DatabaseTestObject(this.getConnection(), this.createLogger(),
-        this.getFullyQualifiedTestClassName()));
+    this.setTestObject(new DatabaseTestObject(this.getConnection(), this.createLogger(), this.getFullyQualifiedTestClassName()));
   }
 
 }
