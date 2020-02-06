fe1361a04531ce9f6f4e323f63fa122b0570e926
Jon Reding <jonr@magenic.com>
2020-02-05 22:43:54 -0600

    Updated UIFind Tests to be up to date with MAQS

--- Framework/jmaqs-selenium/src/main/java/com/magenic/jmaqs/selenium/UIFind.java
+++ Framework/jmaqs-selenium/src/main/java/com/magenic/jmaqs/selenium/UIFind.java
@@ -63,6 +63,16 @@ public class UIFind {
    * Finds all elements using the by provided.
    *
    * @param by             Css Selector
+   * @return The Web Element
+   */
+  public List<WebElement> findElements(By by) {
+    return this.getElementList(by, true);
+  }
+
+  /**
+   * Finds all elements using the by provided.
+   *
+   * @param by             Css Selector
    * @param throwException optional assert parameter - throws an assert exception if no element is found
    * @return The Web Element
    */
@@ -150,8 +160,7 @@ public class UIFind {
    * @param throwException optional assert parameter - throws an exception if no element is found
    * @return The index of the Web Element in the inputted WebElement Collection
    */
-  public int findIndexOfElementWithText(List<WebElement> list, String text,
-      boolean throwException) {
+  public int findIndexOfElementWithText(List<WebElement> list, String text, boolean throwException) {
     int index = -1;
 
     if (list == null) {
@@ -163,7 +172,7 @@ public class UIFind {
     }
 
     for (int i = 0; i < list.size(); i++) {
-      if (list.get(i).toString().equals(text)) {
+      if (list.get(i).getText().equals(text)) {
         return i;
       }
     }
@@ -198,7 +207,7 @@ public class UIFind {
   private List<WebElement> getElementList(By by, boolean throwException) {
     List<WebElement> elems = this.searchItem.findElements(by);
 
-    if (elems.isEmpty() || !throwException) {
+    if (!elems.isEmpty() || !throwException) {
       return elems;
     }
 
