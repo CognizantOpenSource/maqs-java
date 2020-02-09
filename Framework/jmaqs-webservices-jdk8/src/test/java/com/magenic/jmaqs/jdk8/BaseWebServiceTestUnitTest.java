300a2c95e4126463aacb283b51d6490905abd397
Jason Edstrom <JasonE@magenic.com>
2020-02-09 12:05:18 -0600

    renamed module and package

---
+++ Framework/jmaqs-webservices-jdk8/src/test/java/com/magenic/jmaqs/jdk8/BaseWebServiceTestUnitTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2020 (C) Magenic, All rights Reserved
+ */
+
+package com.magenic.jmaqs.jdk8;
+
+import com.magenic.jmaqs.jdk8.BaseWebServiceTest;
+import com.magenic.jmaqs.utilities.helper.TestCategories;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.URISyntaxException;
+
+public class BaseWebServiceTestUnitTest extends BaseWebServiceTest {
+
+  @Test(groups = TestCategories.WebService)
+  public void testGetWebServiceDriver() {
+    Assert.assertNotNull(this.getWebServiceDriver());
+  }
+
+  @Test(groups = TestCategories.WebService)
+  public void testGetWebServiceTestObject() {
+    Assert.assertNotNull(this.getTestObject());
+  }
+
+  @Test(groups = TestCategories.WebService)
+  public void testSetWebServiceDriver() {
+    int hashCode = this.getWebServiceDriver().hashCode();
+    try {
+      this.setWebServiceDriver(this.getWebServiceClient());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    int hashCode1 = this.getWebServiceDriver().hashCode();
+    Assert.assertNotEquals(hashCode, hashCode1);
+  }
+
+  @Test(groups = TestCategories.WebService)
+  public void testGetWebServiceClient() throws URISyntaxException {
+    Assert.assertNotNull(this.getWebServiceClient());
+  }
+}
