80b5d165a992bb17577b0e8efcb7100fad6203a7
Robert Wroblewski <r-wroblewski@bethel.edu>
2020-10-09 14:30:23 -0500

    * Added unit test for setting property being found by PropertyManager

--- jmaqs-utilities/src/test/java/com/magenic/jmaqs/utilities/helper/PropertyManagerUnitTest.java
+++ jmaqs-utilities/src/test/java/com/magenic/jmaqs/utilities/helper/PropertyManagerUnitTest.java
@@ -1,5 +1,7 @@
 package com.magenic.jmaqs.utilities.helper;
 
+import org.junit.Before;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -9,12 +11,32 @@ public class PropertyManagerUnitTest {
     /**
      * An invalid key for the property manager to invoke failure
      */
-    private String INVALID_KEY = "does-not-exist";
+    private static final String INVALID_KEY = "does-not-exist";
+
+    /**
+     * A valid key for the property manager
+     */
+    private static final String VALID_KEY = "does-exist";
 
     /**
      * The default value provided to the property manager
      */
-    private String DEFAULT_VALUE = "default";
+    private static final String DEFAULT_VALUE = "default";
+
+    /**
+     * Valid value for the VALID_KEY property
+     */
+    private static final String VALID_VALUE = "TestLocation";
+
+    @BeforeTest
+    public void setValidSystemProperty() {
+        System.setProperty(VALID_KEY, VALID_VALUE);
+    }
+
+    @Test
+    public void testPropertySetIsMaintained() {
+        assertEquals(PropertyManager.get(VALID_KEY), VALID_VALUE);
+    }
 
     @Test
     public void testPropertyNotSetReturnsNull() {
