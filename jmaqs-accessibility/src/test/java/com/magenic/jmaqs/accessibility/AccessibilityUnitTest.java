5e60921703a3128085a50e628f8e59f0ed8b6a85
Jon Reding <jonr@magenic.com>
2021-03-04 22:46:12 -0600

    cleaned up testing assertions

--- jmaqs-accessibility/src/test/java/com/magenic/jmaqs/accessibility/AccessibilityUnitTest.java
+++ jmaqs-accessibility/src/test/java/com/magenic/jmaqs/accessibility/AccessibilityUnitTest.java
@@ -14,14 +14,14 @@ import com.magenic.jmaqs.selenium.factories.UIWaitFactory;
 import com.magenic.jmaqs.utilities.helper.TestCategories;
 import com.magenic.jmaqs.utilities.logging.FileLogger;
 import com.magenic.jmaqs.utilities.logging.MessageType;
-import org.testng.Assert;
-import org.testng.annotations.Test;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Collectors;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class AccessibilityUnitTest extends BaseSeleniumTest {
   /**
