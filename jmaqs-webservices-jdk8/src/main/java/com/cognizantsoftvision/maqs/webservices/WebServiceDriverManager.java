30d6f29b14cd3788da10d0e1ed3ddcea101e85c7
Jason Edstrom <jason.edstrom@softvision.com>
2022-01-28 13:05:02 -0600

    Package change for WebService modules

---
+++ jmaqs-webservices-jdk8/src/main/java/com/cognizantsoftvision/maqs/webservices/WebServiceDriverManager.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2021 (C) Magenic, All rights Reserved
+ */
+
+package com.cognizantsoftvision.maqs.webservices;
+
+import com.cognizantsoftvision.maqs.base.BaseTestObject;
+import com.cognizantsoftvision.maqs.base.DriverManager;
+import java.util.function.Supplier;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+/**
+ * Web Service Driver Manager Class.
+ */
+public class WebServiceDriverManager extends DriverManager<CloseableHttpClient> {
+  /**
+   * Web Service Driver variable.
+   */
+  private WebServiceDriver webServiceDriver;
+
+  /**
+   * Instantiates a new Web Service Driver Manager.
+   *
+   * @param getDriverFunction Function that specifies how to get the driver.
+   * @param baseTestObject    The Base Test Object.
+   */
+  public WebServiceDriverManager(Supplier<CloseableHttpClient> getDriverFunction, BaseTestObject baseTestObject) {
+    super(getDriverFunction, baseTestObject);
+  }
+
+  /**
+   * Instantiates a new Web Service Driver Manager.
+   *
+   * @param driver         Web Service Driver
+   * @param baseTestObject The Base Test Object.
+   */
+  public WebServiceDriverManager(WebServiceDriver driver, BaseTestObject baseTestObject) {
+    super(driver::getHttpClient, baseTestObject);
+    this.webServiceDriver = driver;
+  }
+
+  /**
+   * Get the Web Service Driver.
+   *
+   * @return The Web Service Driver.
+   */
+  public WebServiceDriver getWebServiceDriver() {
+    // Create default Web Service Driver if null.
+    if (this.webServiceDriver == null) {
+      this.webServiceDriver = new WebServiceDriver(getBase());
+    }
+
+    return this.webServiceDriver;
+  }
+
+  /**
+   * Overrides the Web Service Driver.
+   *
+   * @param driver Web Service Driver
+   */
+  public void overrideDriver(WebServiceDriver driver) {
+    this.webServiceDriver = driver;
+  }
+
+  /**
+   * Close Method sets Base Driver to Null.
+   */
+  public void close() {
+    this.setBaseDriver(null);
+  }
+}
---
+++ jmaqs-webservices-jdk8/src/test/java/com/cognizantsoftvision/maqs/webservices/WebServiceDriverRequestUnitTest/WebServiceDriverDeleteUnitTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021 (C) Magenic, All rights Reserved
+ */
+
+package com.cognizantsoftvision.maqs.webservices.WebServiceDriverRequestUnitTest;
+
+import com.cognizantsoftvision.maqs.utilities.helper.TestCategories;
+import com.cognizantsoftvision.maqs.webservices.BaseWebServiceTest;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.entity.ContentType;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Tests the web service driver Delete functionality.
+ */
+public class WebServiceDriverDeleteUnitTest extends BaseWebServiceTest {
+  /**
+   * Verify delete works.
+   *
+   * @throws Exception
+   *           There was a problem with the test
+   */
+  @Test(groups = TestCategories.WEB_SERVICE)
+  public void webServiceDelete() throws Exception {
+    CloseableHttpResponse response = this.getWebServiceDriver()
+        .deleteContent("/api/XML_JSON/Delete/1", ContentType.TEXT_PLAIN, false);
+    Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+  }
+
+  /**
+   * Verify delete works.
+   *
+   * @throws Exception
+   *           There was a problem with the test
+   */
+  @Test(groups = TestCategories.WEB_SERVICE)
+  public void webServiceStringDelete() throws Exception {
+    CloseableHttpResponse response = this.getWebServiceDriver()
+        .deleteContent("/api/String/Delete/1", ContentType.TEXT_PLAIN, false);
+    Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+  }
+
+  /**
+   * Verify delete throws an error.
+   *
+   * @throws Exception
+   *           There was a problem with the test
+   */
+  @Test(groups = TestCategories.WEB_SERVICE)
+  public void webServiceStringDeleteError() throws Exception {
+    CloseableHttpResponse response = this.getWebServiceDriver()
+        .deleteContent("/api/String/Delete/", ContentType.TEXT_PLAIN, false);
+    Assert.assertEquals(response.getStatusLine().getStatusCode(), 405);
+  }
+}
