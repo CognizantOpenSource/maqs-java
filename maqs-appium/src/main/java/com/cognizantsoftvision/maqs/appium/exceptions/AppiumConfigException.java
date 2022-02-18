03e7f3b654b219009bb56e81d9c036cac9e8ae1e
Jason Edstrom <97257928+jedstromcsv@users.noreply.github.com>
2022-02-18 15:25:47 -0600

    Transition to local database hosted in docker (#48)
    * Package change for Base module
    
    * Package change for Selenium module
    
    * Package change for Utilities module
    
    * Package change for Cucumber module
    
    * Package change for Database module
    
    * Package change for Database module
    
    * Package change for Appium module
    
    * Package change for WebService modules
    
    * Module rename - Base
    
    * Module rename - Utilities
    
    * Module rename - Appium
    
    * Module rename - Selenium
    
    * Module rename - Database
    
    * Module rename - Cucumber
    
    * Module rename - Accessibility
    
    * Module rename - WebServices
    
    * Module rename - Jacoco
    
    * Switched over POM module references
    
    * Copyright year and company changes
    
    * Supporting config file changes
    
    * updated checkstyle docs
    
    * got rid of outdated import statement
    
    * corrected order of import statements
    
    * corrected order of import statements
    
    * updated base unit test output
    
    * updated failing accessibility unit tests file path
    
    * updated imports on HtmlReporter.java
    
    * updated database issues
    
    * updated configs MagenicMaqs tag to GlobalMaqs
    
    * updated configs
    added back content that was there before
    
    * CodeOwners modified for new repo
    
    * docker components moved over from maqs-dotnet
    
    * disabled Mail and Mongo set up until needed
    
    * changed the connection string
    
    * unit test fixes
    
    * temporarily disabled webservices to complete builds
    
    * made the version number static for new release
    
    Co-authored-by: jredingcvs <97762469+jredingcvs@users.noreply.github.com>

---
+++ maqs-appium/src/main/java/com/cognizantsoftvision/maqs/appium/exceptions/AppiumConfigException.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2022 (C) Cognizant SoftVision, All rights Reserved
+ */
+
+package com.cognizantsoftvision.maqs.appium.exceptions;
+
+public class AppiumConfigException extends RuntimeException {
+  public AppiumConfigException(Exception e) {
+    super(e);
+  }
+}
