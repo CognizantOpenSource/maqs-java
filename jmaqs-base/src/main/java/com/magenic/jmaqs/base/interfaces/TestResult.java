29aa53361a04da87a058e16e40da8722b2ce3729
jredingcvs <97762469+jredingcvs@users.noreply.github.com>
2022-05-06 12:34:49 -0500

    delete old base content, put junit content in the new maqs base

--- jmaqs-base/src/main/java/com/magenic/jmaqs/base/interfaces/TestResult.java
+++
@@ -1,9 +0,0 @@
-package com.magenic.jmaqs.base.interfaces;
-
-import com.magenic.jmaqs.utilities.logging.TestResultType;
-
-public interface TestResult {
-    TestResultType getStatus();
-
-    boolean isSuccess();
-}
