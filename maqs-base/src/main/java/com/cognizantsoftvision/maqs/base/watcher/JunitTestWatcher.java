01a0ab254775d8acd0d522527b47d92fcc964be9
jredingcvs <97762469+jredingcvs@users.noreply.github.com>
2022-08-11 15:00:55 -0500

    checkstyle updates

--- maqs-base/src/main/java/com/cognizantsoftvision/maqs/base/watcher/JunitTestWatcher.java
+++ maqs-base/src/main/java/com/cognizantsoftvision/maqs/base/watcher/JunitTestWatcher.java
@@ -15,24 +15,24 @@ import org.junit.jupiter.api.extension.TestWatcher;
  */
 public class JunitTestWatcher implements TestWatcher {
 
-    /**
-     * Test instance of the test being executed per each thread
-     */
-    BaseTest testInstance;
+  /**
+  * Test instance of the test being executed per each thread
+  */
+  BaseTest testInstance;
 
-    public JunitTestWatcher(BaseTest testInstance) {
-        this.testInstance = testInstance;
-    }
+  public JunitTestWatcher(BaseTest testInstance) {
+    this.testInstance = testInstance;
+}
 
-    @Override
-    public void testSuccessful(ExtensionContext context) {
-        this.testInstance.setTestResult(new JunitTestResult(TestResultType.PASS), context);
-        this.testInstance.teardownJunit();
-    }
+  @Override
+  public void testSuccessful(ExtensionContext context) {
+    this.testInstance.setTestResult(new JunitTestResult(TestResultType.PASS), context);
+    this.testInstance.teardownJunit();
+  }
 
-    @Override
-    public void testFailed(ExtensionContext context, Throwable cause) {
-        this.testInstance.setTestResult(new JunitTestResult(TestResultType.FAIL), context);
-        this.testInstance.teardownJunit();
-    }
+  @Override
+  public void testFailed(ExtensionContext context, Throwable cause) {
+    this.testInstance.setTestResult(new JunitTestResult(TestResultType.FAIL), context);
+    this.testInstance.teardownJunit();
+  }
 }
