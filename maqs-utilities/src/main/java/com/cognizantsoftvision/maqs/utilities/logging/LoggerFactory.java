428049848fdc2b0155f53d3c2ca7c802ba2a5d28
jredingcvs <97762469+jredingcvs@users.noreply.github.com>
2022-10-16 13:36:14 -0500

    Checkstyle fixes

--- maqs-utilities/src/main/java/com/cognizantsoftvision/maqs/utilities/logging/LoggerFactory.java
+++ maqs-utilities/src/main/java/com/cognizantsoftvision/maqs/utilities/logging/LoggerFactory.java
@@ -16,13 +16,13 @@ public class LoggerFactory {
 
   }
 
-   /**
+  /**
    * Get a new console logger which respects current logging level.
    * @return A console logger
    */
-    public static ILogger getConsoleLogger() {
-      return new ConsoleLogger(LoggingConfig.getLoggingLevelSetting());
-    }
+  public static ILogger getConsoleLogger() {
+    return new ConsoleLogger(LoggingConfig.getLoggingLevelSetting());
+  }
 
   /**
    * Get a logger.
@@ -30,14 +30,15 @@ public class LoggerFactory {
    * @return A logger
    * @throws MaqsLoggingConfigException if an error occurs
    */
-    public static ILogger getLogger(String logName) throws MaqsLoggingConfigException {
-      String console = "CONSOLE";
+  public static ILogger getLogger(String logName) throws MaqsLoggingConfigException {
+    String console = "CONSOLE";
 
-      if (LoggingConfig.getLoggingEnabledSetting() == LoggingEnabled.NO) {
-        return getLogger(logName, console, MessageType.SUSPENDED);
-      }
+    if (LoggingConfig.getLoggingEnabledSetting() == LoggingEnabled.NO) {
+      return getLogger(logName, console, MessageType.SUSPENDED);
+    }
 
-      return getLogger(logName, Config.getGeneralValue("LogType", console), LoggingConfig.getLoggingLevelSetting());
+    return getLogger(logName, Config.getGeneralValue("LogType", console),
+        LoggingConfig.getLoggingLevelSetting());
     }
 
   /**
@@ -48,21 +49,21 @@ public class LoggerFactory {
    * @return A logger
    * @throws MaqsLoggingConfigException if an exception occurs
    */
-   public static ILogger getLogger(String logName, String logType, MessageType loggingLevel)
-       throws MaqsLoggingConfigException {
-     String logDirectory = LoggingConfig.getLogDirectory();
+  public static ILogger getLogger(String logName, String logType, MessageType loggingLevel)
+      throws MaqsLoggingConfigException {
+    String logDirectory = LoggingConfig.getLogDirectory();
 
-     switch (logType.toUpperCase()) {
-       case "CONSOLE":
-         return new ConsoleLogger(loggingLevel);
-       case "TXT":
-       case "TEXT":
-         return new FileLogger(logDirectory, logName, loggingLevel);
-       case "HTML":
-       case "HTM":
-         return new HtmlFileLogger(logDirectory, logName, loggingLevel);
-       default:
-         throw new MaqsLoggingConfigException("Log type '" + logType + "' is not a valid option");
+    switch (logType.toUpperCase()) {
+     case "CONSOLE":
+       return new ConsoleLogger(loggingLevel);
+     case "TXT":
+     case "TEXT":
+       return new FileLogger(logDirectory, logName, loggingLevel);
+     case "HTML":
+     case "HTM":
+       return new HtmlFileLogger(logDirectory, logName, loggingLevel);
+     default:
+       throw new MaqsLoggingConfigException("Log type '" + logType + "' is not a valid option");
       }
     }
 }
