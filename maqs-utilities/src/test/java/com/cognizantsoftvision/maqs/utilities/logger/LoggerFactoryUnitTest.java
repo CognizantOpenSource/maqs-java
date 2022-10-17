8904fcb3b1a7a2217a47e5fb345c5e14dcc68a84
jredingcvs <97762469+jredingcvs@users.noreply.github.com>
2022-10-17 12:46:22 -0500

    add testing coverage

---
+++ maqs-utilities/src/test/java/com/cognizantsoftvision/maqs/utilities/logger/LoggerFactoryUnitTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 (C) Cognizant SoftVision, All rights Reserved
+ */
+
+package com.cognizantsoftvision.maqs.utilities.logger;
+
+import com.cognizantsoftvision.maqs.utilities.helper.Config;
+import com.cognizantsoftvision.maqs.utilities.helper.TestCategories;
+import com.cognizantsoftvision.maqs.utilities.helper.exceptions.MaqsLoggingConfigException;
+import com.cognizantsoftvision.maqs.utilities.logging.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * The Logger Factory unit test class.
+ */
+public class LoggerFactoryUnitTest {
+
+  @Test(groups = TestCategories.UTILITIES)
+  public void testGetLogger() {
+    HashMap<String, String> newValueMap = new HashMap<>();
+    newValueMap.put("Log", "NO");
+    Config.addGeneralTestSettingValues(newValueMap, true);
+    Assert.assertNotNull(LoggerFactory.getLogger("Test Name"));
+  }
+
+  @Test(groups = TestCategories.UTILITIES)
+  public void testGetConsoleLogger() {
+    Assert.assertNotNull(LoggerFactory.getConsoleLogger());
+    ILogger logger = LoggerFactory.getLogger("logName", "CONSOLE", MessageType.WARNING);
+    Assert.assertTrue(logger instanceof ConsoleLogger);
+  }
+
+  @Test(groups = TestCategories.UTILITIES)
+  public void testGetFileLogger() {
+    List<String> logType = new ArrayList<>();
+    logType.add("TXT");
+    logType.add("TEXT");
+
+    for (String log : logType) {
+      ILogger logger = LoggerFactory.getLogger("logName", log, MessageType.ACTION);
+      Assert.assertTrue(logger instanceof FileLogger);
+    }
+  }
+
+  @Test(groups = TestCategories.UTILITIES)
+  public void testGetHtmlFileLogger() {
+    List<String> logType = new ArrayList<>();
+    logType.add("HTML");
+    logType.add("HTM");
+
+    for (String log : logType) {
+      ILogger logger = LoggerFactory.getLogger("logName", log, MessageType.ACTION);
+      Assert.assertTrue(logger instanceof HtmlFileLogger);
+    }
+  }
+
+  @Test(groups = TestCategories.UTILITIES, expectedExceptions = MaqsLoggingConfigException.class)
+  public void testGetInvalidLogger() {
+    LoggerFactory.getLogger("logName", "TEST", MessageType.ACTION);
+  }
+}
